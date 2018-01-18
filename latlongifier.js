053aa3573085c485a5fa92a22e69ccd4a3ac8c30
Cory Grunkemeyer <cory.grunkemeyer@robotsandpencils.com>
2018-01-18 15:46:20 -0700

    latlongifier outputs directly to map.js

--- latlongifier.js
+++ latlongifier.js
@@ -1,28 +1,9 @@
 const fs = require('fs')
 const dotenv = require('dotenv').config()
 const limit = require("simple-rate-limiter")
-const request = limit(require('request')).to(10).per(1000)
+const request = limit(require('request')).to(1).per(100)
 let googleApiKey = process.env.GOOGLE_API_KEY
-let cityCountryObj = [
-  {
-      "country": "United States",
-      "region": "California",
-      "city": "Los Angeles",
-      "status": "active"
-  },
-  {
-      "country": "United States",
-      "region": "Colorado",
-      "city": "Castle Rock",
-      "status": "trialing"
-  },
-  {
-      "country": "United States",
-      "region": "New York",
-      "city": "New York",
-      "status": "active"
-  }
-]
+let cityCountryObj = [] // JSON from Intercom export goes here
 
 let latLongObj = []
 
@@ -43,32 +24,43 @@ let getLatLong = function (city, region, country, callback) {
     uri: googleApiUrl
   },
   function (error, response, body) {
+    cityCount++
     let returnedObj = JSON.parse(body)
     if (!error && 'results' in returnedObj && returnedObj.results.length && 'geometry' in returnedObj.results[0]) {
-      cityCount++
       let obj = {}
       obj = returnedObj.results[0].geometry.location
       obj.city = city
 
       console.log(cityCount + ' city added: ' + city)
       callback(obj)
+    } else {
+      console.log(returnedObj.error_message + ' Status: ' + returnedObj.status)
     }
   })
 }
 
 let getColor = function (status) {
-    if (status === 'active') {
-        return '#2A9D8F'
-    } else {
-        return '#F4A261'
-    }
+  if (status === 'active') {
+      return '#22DCC5'
+  } else {
+      return '#D37226'
+  }
 }
 
-fs.truncate('cities.json', 0, function(){ console.log('Cleared file contents.') })
-let stream = fs.createWriteStream('cities.json', {flags:'a'})
+fs.truncate('map.js', 0, function(){ console.log('Cleared file contents.') })
+
+let stream = fs.createWriteStream('map.js', {flags:'a'})
+
+stream.write('module.exports = {' + '\n')
+stream.write('\tmapCoords: [' + '\n')
 
 cityCountryObj.forEach(cities => {
+  let comma = function () { if (cityCount !== cityCountryObj.length) { return ',' } else {return ''} }
   getLatLong(cities.city, cities.region, cities.country, function(data) {
-    stream.write('{ "city" : "' + data.city + '", "lat" : "' + data.lat + '", "lng" : "' + data.lng + '", "color": "' + getColor(cities.status) + '" },' + "\n")
+    stream.write('\t\t{ "city" : "' + data.city + '", "lat" : "' + data.lat + '", "lng" : "' + data.lng + '", "color": "' + getColor(cities.status) + '" }' + comma() + "\n")
+    if (cityCount === cityCountryObj.length) {
+      stream.write('\t]' + '\n')
+      stream.write('}' + '\n')
+    }
   })
 })
